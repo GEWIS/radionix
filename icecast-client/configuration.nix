32971b2e44e53040d6f9236896b6105bb0099edd
Victor Klomp <victor@gewis.nl>
2025-07-14 22:52:57 +0200

    Initial commit

---
+++ icecast-client/configuration.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  users.users.radio = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialPassword = "RadioVo!";
+  };
+
+  
+  environment.systemPackages = with pkgs; [
+    butt
+  ];
+
+  # Setup gnome
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+
+  services.xserver.desktopManager.gnome.enable = true;
+
+  system.stateVersion = "25.05";
+}
---
+++ icecast-host/configuration.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  users.users.radio = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialPassword = "RadioVo!";
+  };
+
+  # Icecast
+  services.icecast = {
+    enable = true;
+    admin.password = "test";
+    hostname = "localhost";
+    listen.address = "0.0.0.0";
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 8000 ];
+  };
+
+  system.stateVersion = "25.05";
+}
