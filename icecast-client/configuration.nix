6f690afb6c17600ca43ac19529f8a5d438fbc683
Victor Klomp <victor@gewis.nl>
2025-08-14 16:28:52 +0200

    enable all firmware

--- icecast-client/configuration.nix
+++ icecast-client/configuration.nix
@@ -4,7 +4,7 @@
     ./disko.nix
   ];
 
-  networking.wireless.enable = true;
+  hardware.enableAllFirmware = true;
   networking.hostName = "radio-audio-client";
 
   boot.loader.systemd-boot.enable = true;
